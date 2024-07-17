61361a8adcee2b26fcaf8e93b8f973a4b4c37af7
isa95330 <isabellemazecoiffure@gmail.com>
2024-07-17 16:44:19 +0200

    new commt

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import settings, static
 from shop.views import home_view, eshop_view
 
 urlpatterns = [
@@ -11,3 +11,5 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('eshop/', eshop_view, name='eshop'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/shop/url.py
+++ src/shop/url.py
@@ -1,11 +1,14 @@
 # shop/url.py
-
+from django.conf import settings
 from django.urls import path, include
 
 from .views import product_list, product_detail
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     path('products/', product_list, name='product_list'),
     path('products/<int:product_id>/', product_detail, name='product_detail'),
 ]
+
+
