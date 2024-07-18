1e64b00875a2c7d74caf18882ebcb08cd0782aef
isa95330 <isabellemazecoiffure@gmail.com>
2024-07-18 15:52:23 +0200

    cart_view

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -21,3 +22,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in cart"
--- src/shop/views.py
+++ src/shop/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpRequest
-from .models import Product, Category
+from .models import Product, Category, CartItem
+
 
 def home_view(request):
     # Votre logique de vue ici
@@ -29,3 +31,33 @@ def product_list(request: HttpRequest):
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'product_detail.html', {'product': product})
+
+
+@login_required
+def view_cart(request):
+    cart = request.session.get('cart', {})
+    products = []
+    for product_id, quantity in cart.items():
+        product = get_object_or_404(Product, id=product_id)
+        products.append({'product': product, 'quantity': quantity})
+    return render(request, 'cart.html', {'products': products})
+
+# Ajouter un produit au panier
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    cart = request.session.get('cart', {})
+    if product_id in cart:
+        cart[product_id] += 1
+    else:
+        cart[product_id] = 1
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+@login_required
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    if product_id in cart:
+        del cart[product_id]
+    request.session['cart'] = cart
+    return redirect('view_cart')
