72ff099581da545b481357a1f937b268d36c826a
isa95330 <isabellemazecoiffure@gmail.com>
2024-07-23 11:04:34 +0200

    test26

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['15.237.160.93']
 
 # Application definition
 
