5cfa50597d0be39068e7a7ae5e7269ffc64ed37a
isa95330 <isabellemazecoiffure@gmail.com>
2024-07-24 15:58:06 +0200

    static test62

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,4 +3,4 @@ python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn --workers 3 --bind 0.0.0.0:8080 ecommerce.wsgi:application
+python manage.py runserver 0.0.0.0:8000
